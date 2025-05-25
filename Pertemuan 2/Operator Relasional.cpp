41cf5ce8764e0282bae384b9039d7ae7ffe58cd9
njlazzhra <najlatanjung08@gmail.com>
2025-05-25 21:24:25 +0700

    Pertemuan 2: Penggunaan Operator.cpp

--- Pertemuan 2/Operator Relasional.cpp
+++ Pertemuan 2/Operator Relasional.cpp
@@ -8,12 +8,12 @@ float a,b,c,d,e,f,x,y;
 cout<<"Masukkan nilai X: "; cin >> x;
 cout << "Masukkan nilai Y: "; cin >> y; 
 
-a= x==y;
-b= x!=y;
-c = x > y;
-d = x < y;
-e = x >=y;
-f = x <= y;
+a= x==y; //sama dengan
+b= x!=y; //tidak sama dengan
+c = x > y; //lebih besar
+d = x < y; //lebih kecil
+e = x >=y; //lebih besar sama dengan
+f = x <= y; //lebih kecil sama dengan
 
 cout<<endl;
 cout<<"Hasil dari " << x << "==" << y << "=" << a << endl;
