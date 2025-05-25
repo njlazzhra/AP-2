df6dd7cf769abfe9af9fe0f5f6e81b931e4b8253
njlazzhra <najlatanjung08@gmail.com>
2025-05-25 21:39:06 +0700

    Pertemuan 4: Perulangan.cpp

--- Pertemuan 4/latihan1.cpp
+++ Pertemuan 4/latihan1.cpp
@@ -7,7 +7,7 @@ int main(){
     int i;
 
     cout<<"masukkan sebuah kalimat: "; 
-    getline(cin, kalimat);
+    getline(cin, kalimat); //untuk membaca input yang memiliki lebih dari satu karakter
 
     for(i = 0; i < kalimat.length(); i++){
         kalimat[i] = toupper(kalimat[i]);  //fungsi toupper untuk mengapitalkan lowercase
