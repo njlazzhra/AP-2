8dae08c66bfdff3950b6522ac1e18b3c2432b8cc
njlazzhra <najlatanjung08@gmail.com>
2025-05-25 21:42:28 +0700

    Pertemuan 5: Array & Vector.cpp

--- Pertemuan 5/vector.cpp
+++ Pertemuan 5/vector.cpp
@@ -6,7 +6,7 @@ int main(){
     //Vector Declaration & Initialization
     vector<string> nama_karyawan = {"Ali", "Budi", "Cici", "Doni"};
 
-    //Ascending Element of Vector
+    //Ascending Element of Vector, yaitu membuat vector menjadi berurut sesuai keinginan user
     // for (int i = 0; i < nama_karyawan.size(); i++){
     //     cout<<nama_karyawan[i]<<" ";
     // }
@@ -15,15 +15,15 @@ int main(){
     //     cout<<karyawan<<endl;
     // }
 
-    //Add Data to Vector
-    // nama_karyawan.push_back("Imam");
+    //Add Data to Vector, yaitu untuk menambahkan data ke suatu vector
+    // nama_karyawan.push_back("Imam"); //untuk menambah elemen baru ke dalam vector
     // for (int i = 0; i < nama_karyawan.size(); i++){
     //         cout<<nama_karyawan[i]<<" ";
     //     }
 
-    //Delete Data from Vector
-    nama_karyawan.pop_back("Imam");
-    nama_karyawan.erase(nama_karyawan.begin() + 3);
+    //Delete Data from Vector, yaitu untuk menghapus suatu data dalam vector
+    nama_karyawan.pop_back("Imam"); //untuk menghapus elemen terakhir dalam vector
+    nama_karyawan.erase(nama_karyawan.begin() + 3); //untuk menghapus elemen tertentu dalam vector
     for (int i = 0; i < nama_karyawan.size(); i++){
             cout<<nama_karyawan[i]<<" ";
         }
